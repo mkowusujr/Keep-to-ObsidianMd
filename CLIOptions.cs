52a8eb95a7706101bff69afe507ceac4c9832f41
mkowusujr <mrfriiday@gmail.com>
2023-02-27 20:13:16 -0500

    Created cli

---
+++ CLIOptions.cs
@@ -0,0 +1,22 @@
+namespace KeepMd;
+using CommandLine;
+
+class Options {
+    [Value(0, Required = true, HelpText = "Path to keep zip file")]
+    public string? SrcFilePath { get; set; }
+
+    [Option('d', HelpText = "Destination Filepath")]
+    public string? DestFilePath { get; set; }
+
+    [Option('o', HelpText = "The name of the output")]
+    public string? OutputFileName { get; set; }
+
+    [Option('f', "flat", Default = (bool)false, HelpText = "Whether or not the files should be stored in a folder")]
+    public bool FlatOption { get; set; }
+
+    [Option('t', Default = (bool)true, HelpText = "Whether or not to include google keep tags")]
+    public bool? TagsOption { get; set; }
+
+    [Option('g', Default = (bool)false, HelpText = "Whether or not to group the files by tags into folders")]
+    public bool GroupByTagsOption { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,38 @@
-﻿string targetDirectory =
-    @"C:\Users\mokay\Downloads\Keep";
+﻿// string targetDirectory =
+//     @"C:\Users\mokay\Downloads\Keep";
 
-string[] fileEntries = Directory
-    .GetFiles(targetDirectory)
-    .Where(file => file.Contains(".html"))
-    .ToArray();
+// string[] fileEntries = Directory
+//     .GetFiles(targetDirectory)
+//     .Where(file => file.Contains(".html"))
+//     .ToArray();
 
-Directory.CreateDirectory($@"C:\Users\mokay\Source\Repos\Keep-to-ObsidianMd\output\{"mrfriiday"}");
-foreach (string filepath in fileEntries)
-{
-    Console.WriteLine(filepath);
-    var filename = filepath.Split("\\").Last().Replace(".html", string.Empty);
-    await NoteConvert.KeepArchieveToObsidianMd(filepath, filename, "mrfriiday");
+// Directory.CreateDirectory($@"C:\Users\mokay\Source\Repos\Keep-to-ObsidianMd\output\{"mrfriiday"}");
+// foreach (string filepath in fileEntries)
+// {
+//     Console.WriteLine(filepath);
+//     var filename = filepath.Split("\\").Last().Replace(".html", string.Empty);
+//     await NoteConvert.KeepArchieveToObsidianMd(filepath, filename, "mrfriiday");
+// }
+using CommandLine;
+
+namespace KeepMd;
+
+static class Program {
+    static void Main (string[] args) {
+        var parser = new Parser(with => with.EnableDashDash = true);
+        var result = Parser.Default.ParseArguments<Options>(args);
+        
+        result.WithParsed(options =>
+        {
+            Console.WriteLine($"SrcFilepath = {options.SrcFilePath}");
+            Console.WriteLine($"Dest = {options.DestFilePath}");
+            Console.WriteLine($"Output folder name = {options.OutputFileName}");
+            Console.WriteLine($"Tags option = {options.TagsOption}");
+            Console.WriteLine($"Flat option = {options.FlatOption}");
+            Console.WriteLine($"Flat option = {options.FlatOption}");
+            Console.WriteLine($"Group by tags option = {options.GroupByTagsOption}");
+        });
+
+        // run cli options
+    }
 }
